0aa0ea2db1f6d11f12ca7209e96dea8921ec1e0a
Harshit-2906 <125068698+Harshit-2906@users.noreply.github.com>
2024-03-24 15:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let dropdown = document.getElementById("colorSelect");
+
+document.querySelector("input[type='button']").addEventListener("click", function() {
+  let index = dropdown.selectedIndex;
+  dropdown.remove(index);
+});
